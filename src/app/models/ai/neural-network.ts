b62c8a8389ffe36c1ef09a842687bb0cfeefaab4
Curtis Wilson <cj@wilsontech.com>
2018-02-20 21:41:55 -0600

    Implemented getMove for the neural network

--- src/app/models/ai/neural-network.ts
+++ src/app/models/ai/neural-network.ts
@@ -1,40 +1,82 @@
 import {Layer} from './layer';
 import {Move} from '../move';
+import {Neuron} from './neuron';
+import {Coordinate} from '../game-core/coordinate';
+import {forwardRef} from '@angular/core';
+import {AIBoard} from './aiboard';
 
 export class NeuralNetwork {
-    inputLayer: Layer = new Layer(65);
-    hiddenLayer: Layer; // Mean number of input and output layers.
-    outputLayer: Layer = new Layer(1536);
+	inputLayer: Layer = new Layer(65);
+	hiddenLayers: Layer[] = [new Layer(40), new Layer(40)]; // Mean number of input and output layers.
+	outputLayer: Layer = new Layer(192);
 
-    constructor() {}
+	constructor(path?: string) {
+		if (path) {
 
-    static relu(x: number): number {
-      return Math.max(0, x);
-    }
+		} else {
+			// Create randomized layers.
 
-    // TODO: Implement getMove();
-    getMove(boardState: number[]): Move {
-      // Set the value of the input layer neurons.
-      for (let i = 0; i < this.inputLayer.neurons.length; ++i) {
-        this.inputLayer.neurons[i].value = boardState[i];
-      }
+			this.inputLayer = new Layer(65);
 
-      // Activate each of the layers in sequence.
-      this.inputLayer.activate();
-      this.hiddenLayer.activate();
-      this.outputLayer.activate();
+			// Mean number of input and output layers.
+			this.hiddenLayers = [
+				new Layer(60),
+				new Layer(60)];
 
-      // Evaluate the output layer to get an answer.
-      let move: Move = null;
-      for (const neuron of this.outputLayer.neurons) {
-        // TODO: Find max value for move.
-      }
+			this.outputLayer = new Layer(192);
+		}
+	}
 
-      // Train the network.
-      this.outputLayer.train();
-      this.hiddenLayer.train();
-      this.inputLayer.train();
+	static relu(x: number): number {
+		return Math.max(0, x);
+	}
 
-      return move;
-    }
+	getMove(boardState: number[]): Move {
+		// Create a board with the given state.
+		const board: AIBoard = new AIBoard();
+		board.setAIBoardState(boardState);
+
+		// Set the value of the input layer neurons.
+		for (let i = 0; i < this.inputLayer.neurons.length; ++i) {
+			this.inputLayer.neurons[i].value = boardState[i];
+		}
+
+		// Activate each of the layers in sequence.
+		this.inputLayer.activate();
+		for (let i = 0; i < this.hiddenLayers.length; ++i) {
+			this.hiddenLayers[i].activate();
+		}
+		this.outputLayer.activate();
+
+		// Evaluate the output layer to get an answer.
+		let move: Move = null;
+		let value = -1;
+		for (let i = 0; i < this.outputLayer.neurons.length; ++i) {
+			const neuron: Neuron = this.outputLayer.neurons[i];
+			if (!move || neuron.value > value) {
+
+				// Calculate the move coordinates.
+				const fromRow = Math.floor(i / 24);
+				const fromCol = i % 24;
+				const toRow = fromRow + 1;
+				const toCol = fromCol + i % 3 - 1;
+				const tempMove = new Move(new Coordinate(fromRow, fromCol), new Coordinate(toRow, toCol));
+
+				// Check if the move is a valid one.
+				if (board.isValidMove(tempMove)) {
+					move = tempMove;
+					value = neuron.value;
+				}
+			}
+		}
+
+		// Train the network.
+		this.outputLayer.train();
+		for (let i = this.hiddenLayers.length - 1; i >= 0; --i) {
+			this.hiddenLayers[i].train();
+		}
+		this.inputLayer.train();
+
+		return move;
+	}
 }
